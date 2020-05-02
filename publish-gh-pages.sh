fa5b926735cb05800560bd6a0b34c0cdb894c88a
Fuubi <fabriparrillo@hotmail.com>
2020-05-02 17:59:59 +0200

    add github pages publish script

---
+++ publish-gh-pages.sh
@@ -0,0 +1,17 @@
+git checkout gh-pages
+git checkout master -- ./examples/isomorphic-git
+
+cd examples/isomorphic-git
+
+npm i
+npm run-script build
+mv dist/* ../../
+cd ../..
+rm -r examples
+
+git add .
+git commit -m "update github pages"
+git push origin gh-pages
+
+git checkout master
+
